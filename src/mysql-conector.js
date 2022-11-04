b425a9edb36a49af3b0ac846b5aee291318e8a7d
raul <raulfernandez484@gmail.com>
2022-11-04 17:36:21 +0100

    mysql2

--- src/mysql-conector.js
+++ src/mysql-conector.js
@@ -1,18 +1,10 @@
-import mysql from 'mysql';
+import mysql2 from 'mysql2';
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '../config.js';
-const connector = mysql.createConnection({
+
+export const connector = mysql2.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
     port: DB_PORT
 });
-
-const conectar = () => {
-    connector.connect(err => {
-        if (err) throw err;
-        console.log('FUNCIONA');
-    });
-}
-
-export { connector }
