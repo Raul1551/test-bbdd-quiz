d32f540fe3b75c087347fd3462f1a4a0c0153aaa
raul <raulfernandez484@gmail.com>
2022-11-07 10:33:07 +0100

    eliminacion de archivos front

--- web/activaBoton.js
+++
@@ -1,9 +0,0 @@
-function disableSending() {
-    if ($('input[name="valores[]"]:checked').length > 0) {
-        // Al menos hay un checkbox marcado, habilitar botón
-        $('.boton-siguiente').prop('disabled', false);
-    } else {
-        // No hay checkbox marcado, deshabilitar botón
-        $('.boton-siguiente').prop('disabled', true);
-    };
-};
--- web/arrays.test.js
+++
@@ -1,9 +0,0 @@
-test('name', () => {
-    // array
-    expect(Array.isArray(['value'])).toBe(true);
-    // string
-    expect(typeof 'value').toBe('string');
-    // object
-    expect({value: 'value'}).toBeTruthy();
-    expect(typeof {value: 'value'}).toBe('object');
-  })
